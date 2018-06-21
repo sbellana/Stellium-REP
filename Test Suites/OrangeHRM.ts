73b77d8d8e83b96600801a588026673804dd60a6
Stellium <Stellium@10.1.10.88>
2018-06-21 10:20:48 -0500

    Added Test Suite

---
+++ Test Suites/OrangeHRM.ts
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>OrangeHRM</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>f045840f-4798-4f37-acb7-e338dc5ff58a</testSuiteGuid>
+   <testCaseLink>
+      <guid>bbe80a13-e5bb-45d5-8c26-2e95daf60a56</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1c1b8db5-2baf-4194-94f2-da1c117da9ea</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/AddEmployee</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>075fe4b4-6ae4-4391-bd55-e9b72636fd90</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/AddUser</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>141df859-5cf2-4b03-95cb-5c7412ae3955</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/AddVacancy</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c2ce2150-608d-43a4-a354-22abeb13a19e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Logout</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
